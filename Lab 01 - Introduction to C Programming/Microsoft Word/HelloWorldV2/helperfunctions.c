6c1428173ea631b6cbaaaa6da09699fce5c325f1
Dan-huettner <tamika_co@hotmail.com>
2020-06-19 13:13:36 -0700

    switching branches

--- Lab 01 - Introduction to C Programming/Microsoft Word/HelloWorldV2/helperfunctions.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void printLine1() {
-	printf ("%s\n", "Hello World!");
-}
-
-void printLine2() {
-	printf ("%s\n", "This is Computer Science 2280!");
-}
--- Lab 01 - Introduction to C Programming/PDF/HelloWorldV2/helloworld.c
+++
@@ -1,8 +0,0 @@
-#include "helperfunctions.h"
-
-/* The main function. */
-int main() {
-	printLine1();
-	printLine2();
-	return 0;
-}
